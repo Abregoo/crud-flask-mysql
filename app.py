7d8df61e676a11dfb339f79d5df90cedeec4828e
Manuel <carlosd99m@gmail.com>
2021-02-21 23:53:54 -0600

    Ajustando imagen de edición

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template, request, redirect
+from flask import render_template, request, redirect, url_for
 from flaskext.mysql import MySQL
 from flask import send_from_directory
 
